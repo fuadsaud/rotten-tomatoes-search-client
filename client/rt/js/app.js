7e4452caf9b787ff00c8f2290c0451e03069dbb4
Fuad Saud <fuadfsaud@gmail.com>
2015-03-10 21:32:41 -0400

    Fix search field bindings

--- client/rt/js/app.js
+++ client/rt/js/app.js
@@ -1,24 +1,45 @@
-(function(searchResults) {
+(function(movies) {
   window.App = Ember.Application.create({
     LOG_TRANSITIONS: true
   });
 
   App.Router.map(function() {
-    // this.route('search');
-    // this.route('movies');
+    this.route('search');
   });
 
   App.Movie = DS.Model.extend({
     title: DS.attr('string')
   });
 
-  // App.IndexRoute = Ember.Route.extend({
-  //   searchResults: function() {
-  //     return searchResults;
-  //   },
-  //   renderTemplate: function() {
-  //     console.log("SDJfnkjsdfJSDBFJHSDBFJhBSDJHFbJH");
-  //     this.render('index');
-  //   }
-  // });
+  App.IndexRoute = Ember.Route.extend({
+    beforeModel: function() {
+      this.transitionTo('search');
+    }
+  });
+
+  App.SearchRoute = Ember.Route.extend({
+    actions: {
+      search: function() {}
+    },
+    setupController: function(controller) {
+      controller.set('model', movies);
+    }
+  });
+
+  App.SearchController = Ember.ArrayController.extend({
+    queryParams: ['q'],
+    q: null,
+
+    searchResults: function() {
+      const query = this.get('q');
+      const movies  = this.get('model');
+
+      if (query)
+        return movies.filter(function(movie) {
+          return movie.title.match(new RegExp(query, 'i'))
+        });
+      else
+        return [];
+    }.property('q', 'model')
+  });
 })(movies);
