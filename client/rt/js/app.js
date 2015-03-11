3b715cbb5d260377b73e5a1c7edc8a863cf09421
Fuad Saud <fuadfsaud@gmail.com>
2015-03-10 20:24:11 -0400

    Get ember basic routing to work

--- client/rt/js/app.js
+++ client/rt/js/app.js
@@ -1,1 +1,24 @@
-window.App = Ember.Application.create();
+(function(searchResults) {
+  window.App = Ember.Application.create({
+    LOG_TRANSITIONS: true
+  });
+
+  App.Router.map(function() {
+    // this.route('search');
+    // this.route('movies');
+  });
+
+  App.Movie = DS.Model.extend({
+    title: DS.attr('string')
+  });
+
+  // App.IndexRoute = Ember.Route.extend({
+  //   searchResults: function() {
+  //     return searchResults;
+  //   },
+  //   renderTemplate: function() {
+  //     console.log("SDJfnkjsdfJSDBFJHSDBFJhBSDJHFbJH");
+  //     this.render('index');
+  //   }
+  // });
+})(movies);
