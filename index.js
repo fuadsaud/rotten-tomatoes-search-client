1dfd3fe0fdd85e150323b774804e1daff12d6b7e
Fuad Saud <fuadfsaud@gmail.com>
2015-03-12 19:40:52 -0400

    Add express server

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const morgan  = require('morgan');
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.static('.'))
+
+app.listen(process.env.port || 7001);
