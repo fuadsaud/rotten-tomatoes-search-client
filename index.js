0170728e3bee67673d79b9812f8250a2d82cc1eb
Fuad Saud <fuadfsaud@gmail.com>
2015-03-31 02:36:59 -0400

    Log socket info when booting server

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
 
+const port = process.env.PORT || 7001
+
 const app = express()
+
 
 app.use(morgan('dev'))
 app.use(express.static('.'))
@@ -10,4 +13,6 @@ app.get('*', function(req, res) {
   res.sendFile('index.html', { root: '.' })
 })
 
-app.listen(process.env.PORT || 7001)
+console.log('Starting up HTTP server on listening on port ' + port)
+
+app.listen(port)
